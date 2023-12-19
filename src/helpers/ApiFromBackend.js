7518a43568e8292551e4bbd575b0395ed707fcd0
Josue Rojas <josuemonro@gmail.com>
2023-12-19 08:33:16 -0600

    Subida a producción

--- src/helpers/ApiFromBackend.js
+++ src/helpers/ApiFromBackend.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+//Backend en ambiente productivo
 export const apiFromBackend = axios.create({
   baseURL: "https://mts-destinomx.up.railway.app",
   withCredentials: true,
